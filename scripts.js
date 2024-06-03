78a688132fca74c182d3eda12e9944c9f35dbf0d
NoemiZanforlin <150696258+NoemiZanforlin@users.noreply.github.com>
2024-06-03 10:04:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-function toggleDropdown() {
-    var dropdownContent = document.getElementById("dropdown-content");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-}
+
