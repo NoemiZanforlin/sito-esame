858726ae56ba4a8cf4c2b8a8101e913c66af9910
NoemiZanforlin <150696258+NoemiZanforlin@users.noreply.github.com>
2024-08-30 15:14:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,66 @@
+$(document).ready(function() {
+    // Inizializzazione del menu mobile
+    $('.toast-menu-button').on('click', function() {
+        $('.toast-menu').toggleClass('active');
+    });
+
+ // Inizializzazione di Swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 2000, // Cambia slide ogni 2 secondi
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        loop: true,
+        // Disabilita i controlli di navigazione
+        navigation: {
+            nextEl: null,
+            prevEl: null,
+        },
+    });
+	
+	
+	
+	
+	 var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth', // Visualizzazione del mese
+            editable: true, // Permette la modifica degli eventi
+            selectable: true, // Permette la selezione delle date
+            events: generateRandomEvents(), // Genera eventi randomici
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }
+        });
+        calendar.render();
+	
+	// Funzione per generare eventi randomici
+    function generateRandomEvents() {
+        const events = [];
+        const startDate = new Date();
+        startDate.setDate(1); // Inizia dal primo giorno del mese
+        const endDate = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0); // Ultimo giorno del mese
+
+        // Aggiungi eventi occupati a date randomiche
+        for (let i = 0; i < 5; i++) {
+            let randomDate = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+            events.push({
+                title: 'occupata',
+                start: randomDate.toISOString().split('T')[0], // Formato YYYY-MM-DD
+                backgroundColor: '#dda0dd', // Colore per le date occupate
+                borderColor: 'white'
+            });
+        }
+
+        return events;
+    }
+	
+	
+});
 
